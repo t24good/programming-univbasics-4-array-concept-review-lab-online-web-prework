69ab6baed2c310d9fe49d68eb5c9c2716ff94bd8
Tanesha Goodman <t24good@icloud.com>
2019-10-27 13:33:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,8 +4,9 @@ def find_element_index(array, value_to_find)
     if array[count] == value_to_find
       return count
       end 
-      nil 
-  end 
+      count += 1 
+    end
+  nil 
 end 
 
 def find_max_value(array)
